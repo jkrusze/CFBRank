70bb94b4e2dd8e9d394b1b2bc1858d70e4695724
Unknown <jkrusze@umich.edu>
2018-12-23 14:17:26 -0500

    Added functionality to match up teams
    The results are very close to SEC Fans'. The issue was not weighting the number of pass/rush plays a team runs in the points per yard calculation

--- CFBRanking.py
+++ CFBRanking.py
@@ -9,9 +9,10 @@ import numpy
 ################                     FUNCTIONS                   #################
 ##################################################################################
 ##################################################################################
-def get_data(data_type,TeamName):
+def get_data(data_type,selected_team):
     print('\n')
     print('### Getting Data')
+    print('Getting stas for: ', selected_team)
     if data_type == 1:
         if os.path.exists(DataFile):
             print('### Replacing old file','\n')
@@ -20,12 +21,13 @@ def get_data(data_type,TeamName):
         url = 'https://api.collegefootballdata.com/games?year=2018&seasonType=regular'
         GameData = wget.download(url, DataFile)
     if data_type == 2:
-        TeamName_opt2 = TeamName.replace(" ","%20")
+        selected_team2 = selected_team.replace(" ","%20")
+        selected_team2 = selected_team2.replace("&","%26")
         if os.path.exists(DataFile):
             print('### Replacing old file','\n')
             os.remove(DataFile)
         print('### Importing data')
-        url = 'https://api.collegefootballdata.com/games/teams?year=2018&seasonType=regular&team='+TeamName_opt2
+        url = 'https://api.collegefootballdata.com/games/teams?year=2018&seasonType=regular&team='+selected_team2
         GameData = wget.download(url, DataFile)
 ##################################################################################
 ##################################################################################
@@ -49,16 +51,15 @@ def parse_data(data_type):
             return ParsedGameData
 ##################################################################################
 def get_season_avg(selected_team, ParsedGameData):
-    s_stats = [0] * 18
-    opponent_list = [str('--')] * 18
+    s_stats = [0] * 21
+    opponent_list = [str('--')] * 21
     tmp6=0
-    print('FBS Opponents for', selected_team)
     for tmp1 in ParsedGameData:
         tmp2=tmp1['teams']
         FBS_team=True
-        for tmp3 in tmp2:
-            if str(tmp3['conference']) == 'None':
-                FBS_team=False
+#        for tmp3 in tmp2:
+#            if str(tmp3['conference']) == 'None':
+#                FBS_team=False
         if FBS_team:
             for tmp3 in tmp2:
                 if tmp3['school'] == selected_team:
@@ -80,9 +81,13 @@ def get_season_avg(selected_team, ParsedGameData):
                             s_stats[14]=s_stats[14]+float(tmp5['stat'])
                         if tmp5['category'] == 'firstDowns':
                             s_stats[16]=s_stats[16]+int(tmp5['stat'])
+                        if tmp5['category'] == 'rushingAttempts':
+                            s_stats[18]=s_stats[18]+int(tmp5['stat'])
+                        if tmp5['category'] == 'completionAttempts':
+                            tmp20 = tmp5['stat'].split('-')
+                            s_stats[20] = s_stats[20] + int(tmp20[1])
                 else:
                     opponent_list[tmp6]=tmp3['school']
-                    print('Opponent:',tmp3['school'],'Conference:',tmp3['conference'])
                     tmp6=tmp6+1
                     tmp4=tmp3['stats']
                     s_stats[3]=s_stats[3]+int(tmp3['points'])
@@ -103,6 +108,54 @@ def get_season_avg(selected_team, ParsedGameData):
                             s_stats[17]=s_stats[17]+int(tmp5['stat'])
     return s_stats, opponent_list
 ##################################################################################
+def get_opp_adj_data(selected_team):
+# This function gets season data for each of the selected_teams's opponents.
+# Data is then stored in opp_stats (internal only)
+# Values calculated:
+#
+# oyppa_tot = the sum of the averages of Opponent Yards Per Pass Attempt
+# oypra_tot = the sum of the averages of Opponent Yards Per Rush Attempt
+# aoyppa = (passed on)the average of all oponents' oyppa_tot
+# aoypra = (passed on)the average of all oponents' oypra_tot
+# tot_att = total number of attempts
+# fra_att_r = fraction of attempts which are rushing attempts
+# fra_att_p = fraction of attempts which are passing attempts
+# pts_p_a = (passed on) the points scored per yard, no matter how the yard is gained.
+#   this is calculated by taking the points/game and dividing it by yards per play (total).
+#   The second step requires weighing the yards per pass/rush attempt by the number
+#   plays a team runs that is passing/rushing.
+#
+    print('selected team = ',selected_team)
+    get_data(2,selected_team)
+    ParsedGameData = parse_data(2)
+    s_stats, opp_list = get_season_avg(selected_team, ParsedGameData)
+    opp_stats = numpy.zeros((15,21))
+    for x in range(0,s_stats[1]):
+        if opp_list[x] != '--':
+            selected_team = opp_list[x]
+            get_data(2,selected_team)
+            ParsedGameData = parse_data(2)
+            os_stats, o_opp_list = get_season_avg(selected_team, ParsedGameData)
+            opp_stats[x] = os_stats
+        else:
+            print("They ain't playyyd nobodyyy PAWWWLL")
+
+    oyppa_tot=0
+    oypra_tot=0
+    o_games_tot=0
+    for x in range (0,s_stats[1]):
+        oyppa_tot = oyppa_tot + opp_stats[x,12]/opp_stats[x,1]
+        oypra_tot = oypra_tot + opp_stats[x,8]/opp_stats[x,1]
+    aoyppa = oyppa_tot / s_stats[1]
+    aoypra = oypra_tot / s_stats[1]
+#
+    tot_att = s_stats[18]+s_stats[20]  # total number of play attempts
+    fra_att_r = s_stats[18] / tot_att  # fraction of plays which are rushing
+    fra_att_p = s_stats[20] / tot_att  # fraction of plays which are passing
+    pts_p_a = (s_stats[2])/((s_stats[8]*fra_att_r+s_stats[12]*fra_att_p)) # points per attempt/yard.
+#
+    return s_stats, aoyppa, aoypra,  fra_att_r, fra_att_p, pts_p_a
+##################################################################################
 # Set data file
 now = datetime.datetime.now()
 
@@ -119,18 +172,21 @@ while running:
     print('2: Get Detailed Stats for a Team')
     print('3: Get Team Season Averages')
     print('4: Get Opponent-Adjusted Stats')
+    print('5: Get Match Up Teams')
     print('99: Exit')
     print('##########################','\n')
     option = int(input('Select Option Now:   '))
 ##################################################################################
     if option == 1:
-        get_data(1,'Florida')
-        ParsedGameData=parse_data(1)
+#        get_data(1,'Florida')
+#        ParsedGameData=parse_data(1)
         TeamName_opt1 = 'Florida'
         while TeamName_opt1 != '99':
             print('\n','Type "99" to return to main menu','\n')
             TeamName_opt1 = str(input('Enter Team Name:  '))
-            if TeamName_opt1 != 'BACK':
+            if TeamName_opt1 != '99':
+                get_data(1,'TeamName_opt1')
+                ParsedGameData=parse_data(1)
                 for opt1_tmp1 in ParsedGameData:
                  home_team = opt1_tmp1.get("home_team")
                  away_team = opt1_tmp1.get("away_team")
@@ -155,12 +211,16 @@ while running:
         get_data(2,selected_team)
         ParsedGameData = parse_data(2)
         s_stats, opp_list = get_season_avg(selected_team,ParsedGameData)
-
         print('\n')
         print('##########################','\n')
+        print('FBS Opponents Played:')
+        for x in range(0,s_stats[1]):
+            print(opp_list[x])
+
+        print('\n')
         print('Total # of Games:', s_stats[1],'\n')
         print('Points Scored Per Game:', round(s_stats[2]/s_stats[1],2), '   Points Given Up Per Game:', round(s_stats[3]/s_stats[1],2),'\n')
-        print('Rushing Yards Per Game:', round(s_stats[10]/s_stats[1],2), 'Rushing Yards Allowed Game:', round(s_stats[11]/s_stats[1],2),'\n')
+        print('Rushing Yards Per Game:', round(s_stats[10]/s_stats[1],2), 'Rushing Yards Per Allowed Game:', round(s_stats[11]/s_stats[1],2),'\n')
         print('AVG. Rushing Yards Per Attempt:', round(s_stats[8]/s_stats[1],2), 'AVG. Rushing Yards Per Attempt Allowed:', round(s_stats[9]/s_stats[1],2),'\n')
         print('Passing Yards Per Game:', round(s_stats[14]/s_stats[1],2), 'Passing Yards Allowed Game:', round(s_stats[15]/s_stats[1],2),'\n')
         print('AVG. Passing Yards Per Attempt:', round(s_stats[12]/s_stats[1],2), 'AVG. Passinging Yards Per Attempt Allowed:', round(s_stats[13]/s_stats[1],2),'\n')
@@ -171,23 +231,67 @@ while running:
 
 ##################################################################################
     if option == 4:
-        selected_team = str(input('Enter Team Name:  '))
-        get_data(2,selected_team)
-        ParsedGameData = parse_data(2)
-        s_stats, opp_list = get_season_avg(selected_team, ParsedGameData)
-        opp_stats = numpy.zeros((15,18))
-        print('s_Stats[1]=',s_stats[1])
-        for x in range(0,s_stats[1]):
-            if opp_list[x] != '--':
-                selected_team = opp_list[x]
-                get_data(2,selected_team)
-                ParsedGameData = parse_data(2)
-                os_stats, o_opp_list = get_season_avg(selected_team, ParsedGameData)
-                opp_stats[x] = os_stats
-            else:
-                print("They ain't playyyd nobodyyy PAWWWELLL")
+        selected_team4 = str(input('Enter Team Name:  '))
 
-#        print(opp_stats)
+        s_stats, aoyppa, aoypra, pts_p_r, pts_p_p = get_opp_adj_data(selected_team4)
+
+        print('Passing Allowed % =',round((s_stats[13]*100/s_stats[1])/aoyppa,3))
+        print('Rushing Allowed % =',round((s_stats[9]*100/s_stats[1])/aoypra,3))
+        print('AVG. Rushing Yards Per Attempt:', round(s_stats[8]/s_stats[1],2), 'AVG. Rushing Yards Per Attempt Allowed:', round(s_stats[9]/s_stats[1],2),'\n')
+        print('AVG. Passing Yards Per Attempt:', round(s_stats[12]/s_stats[1],2), 'AVG. Passinging Yards Per Attempt Allowed:', round(s_stats[13]/s_stats[1],2),'\n')
+
+        print('pts per rush yard: =',pts_p_r,'pts per pass yard =', pts_p_p)
+
+
+##################################################################################
+
+    if option == 5:
+        selected_team51 = str(input('Enter Name of Team 1 :  '))
+        s_stats1, aoyppa1, aoypra1, fra_att_r_1, fra_att_p_1,  pts_p_a1 = get_opp_adj_data(selected_team51)
+        print('\n')
+        print('##########################','\n')
+        selected_team52 = str(input('Enter Name of Team 2 :  '))
+        s_stats2, aoyppa2, aoypra2, fra_att_r_2, fra_att_p_2,  pts_p_a2 = get_opp_adj_data(selected_team52)
+#
+        frac_ra1 = ((s_stats1[9]/s_stats1[1])/aoypra1)  # Rush defense, team 1
+        frac_pa1 = ((s_stats1[13]/s_stats1[1])/aoyppa1) # Pass defense, team 1
+        frac_ra2 = ((s_stats2[9]/s_stats2[1])/aoypra2)  # Rush defense, team 2
+        frac_pa2 = ((s_stats2[13]/s_stats2[1])/aoyppa2) # Pass defense, team 2
+#
+        pred_ypra1 = (s_stats1[8]/s_stats1[1]) * frac_ra2   #rush offense, team 1
+        pred_yppa1 = (s_stats1[12]/s_stats1[1]) * frac_pa2  #pass offense, team 1
+        pred_ypra2 = (s_stats2[8]/s_stats2[1]) * frac_ra1   #rush offense, team 2
+        pred_yppa2 = (s_stats2[12]/s_stats2[1]) * frac_pa1  #pass offense, team 2
+#
+        pred_ypplay1 = pred_ypra1 * fra_att_r_1 + pred_yppa1 * fra_att_p_1
+        pred_ypplay2 = pred_ypra2 * fra_att_r_2 + pred_yppa2 * fra_att_p_2
+        pred_pts1 = pred_ypplay1 * pts_p_a1
+        pred_pts2 = pred_ypplay2 * pts_p_a2
+
+
+        print('\n')
+        print('\n')
+        print(selected_team51)
+        print('Passing Allowed % =',round((s_stats1[13]*100/s_stats1[1])/aoyppa1,3))
+        print('Rushing Allowed % =',round((s_stats1[9]*100/s_stats1[1])/aoypra1,3))
+        print('Predicted Yards Per Pass Attempt =',round(pred_yppa1, 3))
+        print('Predicted Yards Per Rush Attempt =',round(pred_ypra1, 3))
+        print('Predicted Yards Per Play =',round(pred_ypplay1, 3))
+        print('Predicted Points Scored =',round(pred_pts1, 3))
+        print('\n')
+        print('\n')
+        print(selected_team52)
+        print('Passing Allowed % =',round((s_stats2[13]*100/s_stats2[1])/aoyppa2,3))
+        print('Rushing Allowed % =',round((s_stats2[9]*100/s_stats2[1])/aoypra2,3))
+        print('Predicted Yards Per Pass Attempt =',round(pred_yppa2, 3))
+        print('Predicted Yards Per Rush Attempt =',round(pred_ypra2, 3))
+        print('Predicted Yards Per Play =',round(pred_ypplay2, 3))
+        print('Predicted Points Scored =',round(pred_pts2, 3))
+        print('\n')
+        print('\n')
+
+
+
 
 
 
